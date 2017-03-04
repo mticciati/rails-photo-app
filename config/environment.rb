f5fd585adef671869ac4c42004cea66af140c03f
mticciati <matthewticciati@gmail.com>
2017-03-04 11:02:57 -0800

    update sendgrid username and password

--- config/environment.rb
+++ config/environment.rb
@@ -8,7 +8,8 @@ ActionMailer::Base.smtp_settings = {
   :address => 'smtp.sendgrid.net',
   :port => '587',
   :authentication => :plain,
-  :api_key => ENV['SENDGRID_API_KEY'],
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
   :domain => 'heroku.com',
   :enable_starttls_auto => true
 }
