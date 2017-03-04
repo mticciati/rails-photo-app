0631d0df4d0b3456a9fa0784a506577013bb47d7
mticciati <matthewticciati@gmail.com>
2017-03-03 21:28:28 -0800

    sendgrid working from local

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,11 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
+ActionMailer::Base.smtp_settings = {
+  :address => 'smtp.sendgrid.net',
+  :port => '587',
+  :authentication => :plain,
+  :api_key => ENV['SENDGRID_API_KEY'],
+  :domain => 'heroku.com',
+  :enable_starttls_auto => true
+}
